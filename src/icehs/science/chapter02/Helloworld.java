5e3aa85548fe690ef34453ce85db10bb43f9109b
admin <admin@10.137.138.162>
2018-01-02 16:53:44 +0900

    Chapter02

---
+++ src/icehs/science/chapter02/Helloworld.java
@@ -0,0 +1,10 @@
+package icehs.science.chapter02;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World~");
+		System.out.println("환영합sl다. 여러분!");
+	}
+
+}
